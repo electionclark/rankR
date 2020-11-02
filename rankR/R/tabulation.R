98ed98183252d8f836652e04353992cb2f6fa52e
Jesse Clark <jtclark@mit.edu>
2020-11-02 16:28:38 -0500

    Added tabulator function. works extremely quickly.

--- rankR/R/tabulation.R
+++ rankR/R/tabulation.R
@@ -2,45 +2,53 @@
 
 maine<- readRDS("maine_rcv.rData")
 
-#rcv_tabulator<- function(data){
-
-data_facsim<- maine ### make facsimile of maine
-
-round<- as.data.frame(table(data_facsim$choice_1))
-round$Var1<- as.character(round$Var1)
-colnames(round)[1]<- "Candidate"
-colnames(round)[2]<- "votes"
-cands<- unique(round$Candidate)
-cands<- cands[!cands %in% c("undervote", "overvote")]
-
-round$initial_percent<- round$votes / nrow(maine)
-
-data_facsim$choice_1<-ifelse(data_facsim$choice_1 == "undervote",  data_facsim$choice_2, data_facsim$choice_1)
-data_facsim<-data_facsim[!data_facsim$choice_1 == "overvote",]
-data_facsim<-data_facsim[!data_facsim$choice_1 == "undervote",]
-round
-
-
-data_facsim$initialvote<-data_facsim$choice_1
-
-loser<- round$Candidate[which.min(round$votes[round$Candidate %in% cands])]
-data_facsim$loser <- ifelse(data_facsim$initialvote == loser, 1, 0)
-data_facsim$choice_1<- ifelse(data_facsim$loser == 1, data_facsim$choice_2, data_facsim$choice_1)
-data_facsim$choice_2<- ifelse(data_facsim$loser == 1, data_facsim$choice_3, data_facsim$choice_2)
-data_facsim$choice_3<- ifelse(data_facsim$loser == 1, "exhausted", data_facsim$choice_3)
 
 
 
-i = 1
-round2<- as.data.frame(table(data_facsim$choice_1))
-alloc<- paste0("round", i)
-colnames(round2)[2]<- alloc
-round2$Var1 <- as.character(round2$Var1 )
-round2$round1[round2$Var1 == "overvote"] <- round2$round1[round2$Var1 == loser] + round2$round1[round2$Var1 == "overvote"]
-round2$round1[round2$Var1 == loser]<- 0
+tabulatR<- function(data, firstrank, finalrank, totalranks){
+  data_facsim<- data ### make facsimile of maine
+
+  #### initial tabulation
+  data_facsim[,firstrank]<-ifelse(data_facsim[,firstrank] == "undervote",  data_facsim[,(firstrank+1)], data_facsim[,firstrank])
+  data_facsim<-data_facsim[!data_facsim[,firstrank] == "overvote",]
+  data_facsim<-data_facsim[!data_facsim[,firstrank] == "undervote",]
+
+  data_facsim$initialvote<-data_facsim[,firstrank]
+
+  counts<- list()
+
+  for(i in 1:(totalranks-1)){
+
+    count<-as.data.frame(table(data_facsim[,firstrank]))
+    count$Var1<- as.character(count$Var1)
+    colnames(count)[1]<- "Candidate"
+    colnames(count)[2]<- "votes"
+    counts[[i]]<- count
+
+    cands<- unique(count$Candidate)
+    cands<- cands[!cands %in% c("undervote", "overvote")]
+    loser<- count$Candidate[which.min(count$votes[count$Candidate %in% cands])]
+    data_facsim$loser<- ifelse(data_facsim[,firstrank] == loser, 1, 0)
+    data_facsim[,firstrank]<- ifelse(data_facsim$loser == 1, data_facsim[,(firstrank +1)], data_facsim[,firstrank])
+
+    for(j in 1:(totalranks-1)){
+      #### loop through and reallocate here
+      data_facsim[,(firstrank+ j)]<- ifelse(data_facsim$loser == 1, data_facsim[,(firstrank+ j + 1)], data_facsim[,(firstrank+ j)])
+    }
+
+    count<-as.data.frame(table(data_facsim[,firstrank]))
+    count$Var1<- as.character(count$Var1)
+    colnames(count)[1]<- "Candidate"
+    colnames(count)[2]<- "votes"
+    count$votes[count$Candidate == loser]<- 0
+    total_roundi<- sum(count$votes[count$Candidate %in% cands])
+    counts[[i]]<- count
+
+    ifelse(length(count$Candidate[(count$votes/ total_roundi) > .5]) > 0, return(counts), print("Next round!"))
+
+  }
+}
 
 
-total_roundi<- sum(round2$round1[round2$Var1 %in% cands])
-
-round2$Var1[(round2$round1 / total_roundi) > .5]
+tabulatR(data = maine, totalranks = 3, firstrank = 4, finalrank = 6)
 
